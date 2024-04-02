a6960ab7fc8fdfcde3691b3c207a608460a0320b
Jonathan Fraine <jdfraine@gmail.com>
2024-04-03 00:15:02 +0200

    refactored rag.py to create objected oriented pipeline and avoid global variables

--- wikidatachat/rag.py
+++ wikidatachat/rag.py
@@ -18,106 +18,83 @@ from .vector_store_interface import (
     setup_document_stream_from_list
 )
 
-# Create logger instance from base logger config in `logger.py`
-logger = get_logger(__name__)
 
-# TODO: create simple object oriented pipeline to minimise global variables
-global embedder
-embedder = None
+class RAGPipeline:
+    def __init__(self, sentence_transformer_model='svalabs/german-gpl-adapted-covid', device='cpu'):
+        self.logger = get_logger(__name__)
+        self.embedder = make_embedder(
+            sentence_transformer_model=sentence_transformer_model, device=device)
 
+    def process_query(self, query: str, top_k: int = 3, lang: str = 'de',
+                      content_key: str = None, meta_keys: list = [],
+                      embedding_similarity_function: str = "cosine"):
+        query_document = Document(content=query)
+        query_embedded = self.embedder.run([query_document])
+        query_embedding = query_embedded['documents'][0].embedding
 
-def rag_pipeline(
-        query: str, top_k: int = 3, lang: str = 'de', device: str = 'cpu',
-        content_key: str = None, meta_keys: list = [],
-        embedding_similarity_function: str = "cosine",
-        sentence_transformer_model: str = 'svalabs/german-gpl-adapted-covid'):
-
-    # TODO: Test if this loads more than once or needs a global variable
-    global embedder
-    if embedder is None:
-        embedder = make_embedder(
-            sentence_transformer_model=sentence_transformer_model,
-            device=device
+        wikidata_statements = get_wikidata_statements_from_query(
+            query,
+            lang='en',
+            timeout=10,
+            n_cores=cpu_count(),
+            verbose=False,
+            api_url='https://www.wikidata.org/w',
+            wikidata_base='"wikidata.org"',
+            serapi_api_key=os.environ.get("SERAPI_API_KEY"),
+            return_list=True
         )
 
-    query_document = Document(content=query)
-    query_embedded = embedder.run([query_document])
-    query_embedding = query_embedded['documents'][0].embedding
+        self.logger.debug(f'{wikidata_statements=}')
+        for wds_ in wikidata_statements:
+            self.logger.debug(f'{wds_=}')
 
-    wikidata_statements = get_wikidata_statements_from_query(
-        query,
-        lang='en',
-        timeout=10,
-        n_cores=cpu_count(),
-        verbose=False,
-        api_url='https://www.wikidata.org/w',
-        wikidata_base='"wikidata.org"',
-        serapi_api_key=os.environ.get("SERAPI_API_KEY"),
-        return_list=True
-    )
+        _, retriever = setup_document_stream_from_list(
+            dict_list=wikidata_statements,
+            content_key=content_key,
+            meta_keys=meta_keys,
+            embedder=self.embedder,
+            embedding_similarity_function=embedding_similarity_function,
+            device='cpu'
+        )
 
-    logger.debug(f'{wikidata_statements=}')
-    for wds_ in wikidata_statements:
-        logger.debug(f'{wds_=}')
+        retriever_results = retriever.run(
+            query_embedding=list(query_embedding),
+            filters=None,
+            top_k=top_k,
+            scale_score=None,
+            return_embedding=None
+        )
 
-    # logger.debug(f'{len(wikidata_statements)=}')
-    # logger.debug(f'{type(wikidata_statements)=}')
+        self.logger.debug('retriever results:')
+        for retriever_result_ in retriever_results['documents']:
+            self.logger.debug(retriever_result_)
 
-    _, retriever = setup_document_stream_from_list(
-        dict_list=wikidata_statements,
-        content_key=content_key,
-        meta_keys=meta_keys,
-        embedder=embedder,
-        embedding_similarity_function=embedding_similarity_function,
-        device=device
-    )
+        system_prompt = system_prompts[lang]
+        user_prompt_builder = user_prompt_builders[lang]
 
-    retriever_results = retriever.run(
-        query_embedding=list(query_embedding),
-        filters=None,
-        top_k=top_k,
-        scale_score=None,
-        return_embedding=None
-    )
+        user_prompt_build = user_prompt_builder.run(
+            question=query_document.content,
+            documents=retriever_results['documents']
+        )
 
-    logger.debug('retriever results:')
-    for retriever_result_ in retriever_results['documents']:
-        logger.debug(retriever_result_)
+        prompt = user_prompt_build['prompt']
+        self.logger.debug(f'{prompt=}')
 
-    system_prompt = system_prompts[lang]
-    user_prompt_builder = user_prompt_builders[lang]
+        messages = [
+            ChatMessage.from_system(system_prompt),
+            ChatMessage.from_user(prompt),
+        ]
 
-    user_prompt_build = user_prompt_builder.run(
-        question=query_document.content,
-        documents=retriever_results['documents']
-    )
+        response = llm.run(messages)
+        self.logger.debug(response)
 
-    prompt = user_prompt_build['prompt']
+        answer_builder = AnswerBuilder()
+        answer_build = answer_builder.run(
+            query=query_document.content,
+            replies=response['replies'],
+            meta=[r.meta for r in response['replies']],
+            documents=retriever_results['documents']
+        )
 
-    logger.debug(f'{prompt=}')
-
-    messages = [
-        ChatMessage.from_system(system_prompt),
-        ChatMessage.from_user(prompt),
-    ]
-
-    response = llm.run(
-        messages,
-        # generation_kwargs={"temperature": 0.2}
-    )
-
-    logger.debug(response)
-
-    answer_builder = AnswerBuilder()
-    answer_build = answer_builder.run(
-        query=query_document.content,
-        replies=response['replies'],
-        meta=[r.meta for r in response['replies']],
-        documents=retriever_results['documents'],
-        pattern=None,
-        reference_pattern=None
-    )
-
-    logger.debug(f'{answer_build=}')
-
-    return answer_build['answers'][0]
+        self.logger.debug(f'{answer_build=}')
+        return answer_build['answers'][0]
