945524e64089c23cba45dd41f0bcb292b5a5bd03
Jonathan Fraine <jdfraine@gmail.com>
2024-04-03 14:10:24 +0200

    tested new object oriented config for LLM configuratoin. This led to a redefinition of requirements in requirements.txt

--- wikidatachat/llm_config.py
+++ wikidatachat/llm_config.py
@@ -47,31 +47,12 @@ config = AppConfig()
 logger.debug(f'Using {config.OLLAMA_MODEL_NAME=}')
 logger.debug(f'Endpoint: {config.OLLAMA_URL=}')
 logger.debug(f'Generate: {config.OLLAMA_CHAT_URL=}')
-logger.debug(f'Timeout: {self.OLLAMA_TIMEOUT=}')
+logger.debug(f'Timeout: {config.OLLAMA_TIMEOUT=}')
 
 logger.info(f"Setting up ollama with {config.OLLAMA_MODEL_NAME}")
 
 llm = OllamaChatGenerator(
     model=config.OLLAMA_MODEL_NAME,
     url=config.OLLAMA_CHAT_URL,
-    timeout=self.OLLAMA_TIMEOUT
+    timeout=config.OLLAMA_TIMEOUT
 )
-"""
-HUGGING_FACE_HUB_TOKEN = os.environ.get('HUGGING_FACE_HUB_TOKEN')
-OLLAMA_MODEL_NAME = os.environ.get("OLLAMA_MODEL_NAME")
-OLLAMA_URL = os.environ.get("OLLAMA_URL")
-OLLAMA_TIMEOUT = os.environ.get("OLLAMA_TIMEOUT", 120)
-
-OLLAMA_CHAT_URL = f"{OLLAMA_URL}/api/chat"
-
-logger.info(f'Using {OLLAMA_MODEL_NAME=}')
-logger.info(f'Endpoint: {OLLAMA_URL=}')
-logger.info(f'Generate: {OLLAMA_CHAT_URL=}')
-
-
-llm = OllamaChatGenerator(
-    model=OLLAMA_MODEL_NAME,
-    url=OLLAMA_CHAT_URL,
-    timeout=OLLAMA_TIMEOUT
-)
-"""
